0cf6ba5903fa644c73bbbae5da5b7b0e76634635
iamrnkchatt <iamrnkchatt@gmail.com>
2022-01-09 21:15:23 +0530

    Portfolio Website made by flutter

---
+++ lib/values/strings.dart
@@ -0,0 +1,207 @@
+part of values;
+
+class StringConst {
+  //strings
+  static const String APP_NAME = "iamrnkchatt";
+
+  //Nav Section
+  static const String NAME_ABBREV = "CA";
+  static const String HOME = "Home";
+  static const String SERVICES = "Services";
+  static const String ABOUT = "About";
+  static const String PROJECTS = "Projects";
+  static const String AWARDS = "Awards";
+  static const String BLOG = "Blog";
+  static const String SKILLS = "Skills";
+  static const String CONTACT = "Contacts";
+
+  static const String CONTACT_ME = "Contact Me";
+  static const String DEV_EMAIL = "iamrnkchatt@gmail.com";
+  static const String EMAIL_URL = "mailto:<$DEV_EMAIL>?subject=&body=";
+
+  //Header Section
+  static const String FIRST_NAME = "Raunak";
+  static const String INTRO = "Hi! I'm raunak Chatterjee -";
+  static const String POSITION = "Web & Mobile Developer";
+  static const String ABOUT_DEV =
+      "Experienced Web & Mobile Application Developer with a demonstrated history of working in the design industry.";
+  static const String ABOUT_ME_1 =
+      "Experienced Web & Mobile Application Developer with";
+  static const String ABOUT_ME_2 =
+      "a demonstrated history of working in the design industry.";
+  static const String DOWNLOAD_CV = "Download CV";
+  static const String EMAIL = "Email";
+  static const String DEV_EMAIL_2 = "iamrnkchatt@mail.com";
+  static const String BEHANCE = "LinkedIn";
+  static const String BEHANCE_ID = "linkedin.com/in/iamrnkchatt";
+  static const String HIRE_ME_NOW = "Hire Me Now";
+  static const String UI_UX = "Web Developer";
+  static const String UI_UX_DESC = "creative websites for you";
+  static const String PHOTOGRAPHER = "App developer";
+  static const String PHOTOGRAPHER_DESC = "Awesome ios & android apps";
+  static const String FREELANCER = "Graphics Designer";
+  static const String FREELANCER_DESC = "You can hire me now";
+
+  static const String PYTHON = "Flutter";
+  static const String JAVA = "PHP";
+  static const String C_PLUS_PLUS = "Laravel";
+  static const String JAVASCRIPT = "Javascript";
+  static const String HTML = "HTML";
+  static const String CSS = "CSS";
+
+  //AboutMe Section
+  static const String HI = "Hi";
+  static const String THERE = "There!";
+  static const String ABOUT_ME = "about me";
+  static const String ABOUT_ME_DESC =
+      "Experienced Web & Mobile Application Developer with a demonstrated history of working in the design industry. Skilled in Mobile Application Development, PHP, WordPress, Web Design, and Adobe Photoshop";
+  static const String ABOUT_ME_DESC_1 =
+      "Experienced Web & Mobile Application Developer with a ";
+  static const String ABOUT_ME_DESC_2 =
+      "demonstrated history of working in the design industry.";
+  static const String ABOUT_ME_DESC_3 =
+      "Skilled in Mobile Application Development, PHP,";
+  static const String ABOUT_ME_DESC_4 =
+      "WordPress, Web Design, and Adobe Photoshop";
+  static const String ABOUT_ME_DESC_5 = "and Adobe Photoshop";
+  static const String CREATIVE_DESIGN = "Need Web or Mobile App?";
+  static const String HELP = "I can Help You!";
+  static const String FOLLOW_ME_1 = "You can also follow me here:";
+  static const String DRIBBLE = "Dribble";
+  static const String INSTA = "Insta";
+
+  //Skills Section
+  static const String MY_SKILLS = "my skills";
+  static const String SKILLS_TITLE_1 = "What My Design";
+  static const String SKILLS_TITLE_2 = "Skills Include";
+  static const String SKILLS_DESC =
+      "These are the words we live by in everything we do. Every story we tell, every brand we build, and every interaction we create must not only look beautiful.";
+  static const String TOOLS_1_NAME = "Flutter";
+  static const String TOOLS_1_PROFICIENCY = "80%";
+  static const String TOOLS_2_NAME = "PHP";
+  static const String TOOLS_2_PROFICIENCY = "90%";
+  static const String TOOLS_3_NAME = "Laravel";
+  static const String TOOLS_3_PROFICIENCY = "70%";
+  static const String SKILLS_1 = "Mobile App";
+  static const String SKILLS_1_DESC =
+      "We believe direct collaboration with our clients is essential to bringing unique";
+  static const String SKILLS_2 = "Web Development";
+  static const String SKILLS_2_DESC =
+      "We believe direct collaboration with our clients is essential to bringing unique";
+  static const String SKILLS_3 = "Graphics Design";
+  static const String SKILLS_3_DESC =
+      "We believe direct collaboration with our clients is essential to bringing unique";
+  static const String SKILLS_4 = "Digital marketer";
+  static const String SKILLS_4_DESC =
+      "We believe direct collaboration with our clients is essential to bringing unique";
+
+  //Stats Section
+  static const String HAPPY_CLIENTS = "Happy Clients";
+  static const String HAPPY_CLIENTS_NUM = "120";
+  static const String YEARS_OF_EXPERIENCE = "Years of Experience";
+  static const String YEARS_OF_EXPERIENCE_NUM = "10";
+  static const String INCREDIBLE_PROJECTS = "Incredible Projects";
+  static const String INCREDIBLE_PROJECTS_NUM = "230";
+  static const String AWARD_WINNING = "Award Winning";
+  static const String AWARD_WINNING_NUM = "18";
+
+  //Projects Section
+  static const String MEET_MY_PROJECTS = "Meet My Projects";
+  static const String MY_WORKS = "my works";
+  static const String PROJECTS_DESC =
+      "Product Designer and Digital Creative Director working in design field for 13 years so far, specialized in UI/UX, Branding and digital designs. These are the words we live by in everything we do.";
+  static const String ALL_PROJECTS = "All Projects";
+  static const String ALL = "All";
+  static const String BRANDING = "Mobile App";
+  static const String PACKAGING = "Web Design";
+  static const String PHOTOGRAPHY = "Graphics Design";
+  static const String WEB_DESIGN = "Viddeo Editing";
+  static const String PORTFOLIO_1_TITLE = "Urban Experience";
+  static const String PORTFOLIO_2_TITLE = "Graphic Design";
+  static const String PORTFOLIO_3_TITLE = "Launch Product";
+  static const String PORTFOLIO_4_TITLE = "3D Animation";
+  static const String PORTFOLIO_5_TITLE = "Collect Idea";
+  static const String PORTFOLIO_6_TITLE = "Company Branding";
+
+  //Awards & Section
+  static const String MY = "My";
+  static const String CV = "CV";
+  static const String MY_AWARDS = "my awards";
+  static const String AWARDS_TITLE = "Honours & Awards";
+  static const String AWARDS_DESC =
+      "Since 2015 I have been a visionary and a reliable software engineering partner for world-class brands.";
+  static const String AWARDS_TYPE_TITLE_1 = "In Press";
+  static const String AWARDS_TYPE_TITLE_2 = "Awards";
+  static const String AWARDS_1 = "Forbes";
+  static const String AWARDS_2 = "New York Times";
+  static const String AWARDS_3 = "Smashing Magazine";
+  static const String AWARDS_4 = "Entrepreneur";
+  static const String AWARDS_5 = "First Magazine";
+  static const String AWARDS_6 = "The Type Direction Club 60";
+  static const String AWARDS_7 = "ClassiCon-Brochure 2017";
+  static const String AWARDS_8 = "The Good Design Awards 2017";
+  static const String AWARDS_9 = "For Print Only-Under consideration";
+  static const String AWARDS_10 = "CI Magazine 2017";
+
+  //Brands Section
+  static const String BRANDS_SECTION_TITLE = "Brands I've worked with";
+  static const String HIRE_ME = "Hire me";
+
+  //Testimonials Section
+  static const String MY_TESTIMONIALS = "my testimonials";
+  static const String TESTIMONIALS_SECTION_TITLE = "Client Testimonials";
+  static const String TESTIMONIALS_1_NAME = "Arnold Wilson";
+  static const String TESTIMONIALS_1_POSITION = "PHP Developer";
+  static const String TESTIMONIALS_1 =
+      "Raunak has quickly become an indispensable partner in capturing the innovation in our products in a way that best represents our brand voice. He bring fresh ideas and challenges to the way our industry typically markets products. An integrated marketing campaign supporting a major 2015 product launch exceeded goals by as much as 300%.";
+  static const String TESTIMONIALS_2_NAME = "Ronald Green";
+  static const String TESTIMONIALS_2_POSITION = "UI Designer";
+  static const String TESTIMONIALS_2 =
+      "What a fantastic day it's been at iamrnkchatt! Huge shoutout to the team for connecting us all and making this amazing event possible. He bring fresh ideas and challenges to the way our industry typically markets products. This year I brought friends. Come say hi! Development is one of our values & a theme of iamrnkchatt talks.";
+
+  //Blog Section
+  static const String MY_BLOG = "my blog";
+  static const String BLOG_SECTION_TITLE_1 = "Read My Own";
+  static const String BLOG_SECTION_TITLE_2 = "Blog. Be Trendy";
+  static const String BLOG_DESC =
+      "We met years ago at an advertising agency where we learned a ton of dos and some don'ts. There were project deadlines, happy hours, client proposals and a few sparks flying.";
+  static const String BLOG_VIEW_ALL = "View All";
+  static const String BLOG_CATEGORY_1 = "inspiration";
+  static const String BLOG_CATEGORY_2 = "design";
+  static const String BLOG_CATEGORY_3 = "creative";
+  static const String BLOG_DATE = "February 4, 2020";
+  static const String BLOG_TITLE_1 = "How Much Does a Website Cost to Build?";
+  static const String BLOG_TITLE_2 = "10 Tips How Create Best Packaging Design";
+  static const String BLOG_TITLE_3 = "How Get Your Visitors Love Your Brand";
+  static const String READ_MORE = "Read More";
+  static const String BLOGGING = "Blogging";
+
+  //Footer Section
+  static const String LETS_TALK = "Let's Talk!";
+  static const String PHONE_ME = "Phone Me";
+  static const String PHONE_NUMBER = "+91 9083195364";
+  static const String MAIL_ME = "Mail Me";
+  static const String FOLLOW_ME_2 = "Follow Me";
+  static const String FOLLOW_ME_2_URL = "dribbble.com/iamrnkchatt";
+  static const String RIGHTS_RESERVED = "© 2022 All rights reserved";
+  static const String BUILT_BY = "Built with Flutter by";
+  static const String DAVID_COBBINA = "Raunak Chatterjee";
+  static const String DESIGNED_BY = "Designed by";
+  static const String WEB_GENIUS_LAB = "iamrnkchatt";
+  static const String MADE_IN_GHANA = "Made in";
+  static const String WITH_LOVE = "with";
+  static const String FIND_ME = "Find me on..";
+
+  //URL Links
+  static const String WEB_GENIUS_LAB_URL = "#";
+  static const String BEHANCE_URL = "#";
+  static const String DRIBBLE_URL = "https://dribbble.com/iamrnkchatt";
+  static const String LINKED_IN_URL =
+      "https://www.linkedin.com/in/iamrnkchatt/";
+  static const String INSTAGRAM_URL = "https://www.instagram.com/iamrnkchatt/";
+  static const String GITHUB_URL = "https://github.com/iamrnkchatt";
+  static const String FACEBOOK_URL = "#";
+  static const String TWITTER_URL = "#";
+  static const String DAVID_LEGEND_URL = "#";
+  static const String DESIGN_URL = "#";
+}
